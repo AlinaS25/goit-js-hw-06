18f2730c248de321e79bb316821bddd52e4eba57
AlinaS25 <115736090+AlinaS25@users.noreply.github.com>
2023-07-12 20:46:57 +0300

    hw-6-changes-2

--- js/task-01.js
+++ js/task-01.js
@@ -1,5 +1,5 @@
 const itemsList = document.querySelectorAll(".item");
-console.log(`Number of categories: $(itemsList.length)`);
+console.log(`Number of categories: ${itemsList.length}`);
 
 itemsList.forEach((item) => {
   console.log(`Categories: ${item.firstElementChild?.textContent}`);
